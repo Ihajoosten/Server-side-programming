62cbdcaefa2c11ef4f53b81e0253ae3293caa5d8
Luc Joosten <lha.joosten@student.avans.nl>
2019-10-31 12:01:12 +0100

    added IRepository for Meal and Dish

---
+++ EasyMeal/Core/Interfaces/IDishRepo.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IDishRepo
+    {
+        IQueryable<Entities.Dish> Dishes { get; }
+
+        List<Dish> GetDishes();
+
+        Dish GetDishById(int? id);
+
+        void CreateDish(Dish dish);
+
+        void UpdateDish(int? id);
+
+        void DeleteDish(int? id);
+    }
+}
---
+++ EasyMeal/Core/Interfaces/IMealRepo.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IMealRepo
+    {
+
+        IQueryable<Meal> Dishes { get; }
+
+        List<Meal> GetMeals();
+
+        Meal GetMealById(int? id);
+
+        bool CreateMeal(Meal dish, Dish[] dishes);
+
+        void UpdateMeal(int? id);
+
+        void DeleteMeal(int? id);
+    }
+}
