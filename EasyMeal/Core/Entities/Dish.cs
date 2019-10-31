462be3ba12cb2160fa8af51c78eca4b6fa77ca32
Luc Joosten <lha.joosten@student.avans.nl>
2019-10-31 11:55:45 +0100

    added some basic entities

---
+++ EasyMeal/Core/Entities/Dish.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Dish
+    {
+        public Dish() { }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Display(Name = "Dish Category")]
+        [Required]
+        public string Type { get; set; }
+
+        [Display(Name = "Diet restriction")]
+        public string Restriction { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public decimal Price { get; set; }
+    }
+}
